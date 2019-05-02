08e6dc724b4971f8dbd416c64f4bdd55b3c60660
Anoop <anooprajan1989@gmail.com>
2019-05-02 18:02:02 +0530

    Added hint functionality

--- app/client/app.js
+++ app/client/app.js
@@ -1,10 +1,10 @@
 
-global.startApp = function(container) {
+global.startApp = function (container) {
   console.log("Here is the container:", container);
 }
 
 
-$(".reset, .reply").click(function() {
+$(".reset, .reply").click(function () {
   var $nodes = $(".diamondsweeper-board").find(".cell");
   shuffle($nodes, "cell");
   $(".diamondsweeper-board").append($nodes);
@@ -18,42 +18,65 @@ $(".reset, .reply").click(function() {
 
 //Shuffle the cards
 function shuffle(nodes, switchableSelector) {
-  var length = nodes.length;  
+  var length = nodes.length;
   var switchable = nodes.filter("." + switchableSelector);
   var switchIndex = [];
 
-  $.each(switchable, function(index, item) {
-     switchIndex[index] = $(item).index(); 
+  $.each(switchable, function (index, item) {
+    switchIndex[index] = $(item).index();
   });
-  
+
   var switchLength = switchIndex.length;
   var randomPick, randomSwap;
-  
+
   for (var index = length; index > 0; index--) {
-      randomPick = switchIndex[Math.floor(Math.random() * switchLength)];      
-      randomSwap = nodes[index - 1];    
-      if($(randomSwap).hasClass(switchableSelector)) {
-          nodes[index - 1] = nodes[randomPick];
-          nodes[randomPick] = randomSwap;
-      }
+    randomPick = switchIndex[Math.floor(Math.random() * switchLength)];
+    randomSwap = nodes[index - 1];
+    if ($(randomSwap).hasClass(switchableSelector)) {
+      nodes[index - 1] = nodes[randomPick];
+      nodes[randomPick] = randomSwap;
+    }
   }
   return nodes;
 }
 
 // Open boxes
-$(".cell").click(function(){
-  $(this).removeClass("unknown");
+$(".cell").click(function () {
 
-  var nextEle = $(this).index();
-  if(indexOf(nextEle).hasClass("cell")){
-    alert("hi");
+  var indexClicked = $(this).index();
+  var indexPrev = $(this).index() - 1;
+  var indexNext = $(this).index() + 1;
+  var indexTop = $(this).index() - 8;
+  var indexBottom = $(this).index() + 8;
+
+  $(".cell").removeClass("diamond-right diamond-left diamond-top diamond-bottom");
+
+  if ($(".cell").eq(indexClicked).hasClass('diamond')) {
+    $(".cell").eq(indexClicked).addClass("found");
+  } else {
+    if ($(".cell").eq(indexNext).hasClass('diamond') && !$(".cell").eq(indexNext).hasClass('found')) {
+      $(".cell").eq(indexClicked).addClass('diamond-right');
+    } else if ($(".cell").eq(indexPrev).hasClass('diamond') && !$(".cell").eq(indexPrev).hasClass('found')) {
+      $(".cell").eq(indexClicked).addClass('diamond-left');
+    } else if ($(".cell").eq(indexTop).hasClass('diamond') && !$(".cell").eq(indexTop).hasClass('found')) {
+      $(".cell").eq(indexClicked).addClass('diamond-top');
+    } else if ($(".cell").eq(indexBottom).hasClass('diamond') && !$(".cell").eq(indexBottom).hasClass('found')) {
+      $(".cell").eq(indexClicked).addClass('diamond-bottom');
+    } else {
+      
+    }
   }
   
-  if($(this).hasClass("diamond")){
-    $(this).addClass("found");
-  }
 
-  if($(".found").length >= 8){
+  $(".cell").click(function(){
+    if ($(this).hasClass('diamond')){
+      $(this).addClass("found");
+    }
+  });
+
+  $(this).removeClass("unknown");
+
+  if ($(".found").length >= 8) {
     $(".success-wrap").addClass("show");
   }
   totalCount(); // Count of total cell opened
@@ -61,18 +84,18 @@ $(".cell").click(function(){
 });
 
 
-$( document ).ready(function() {
+$(document).ready(function () {
   totalCount();
   totalDiamond();
 })
 
-function totalCount(){
+function totalCount() {
   var count = $(".diamondsweeper-board").find(".unknown").length;
   $(".total-value").text(64 - count);
   $(".final-point").text(count);
 }
 
-function totalDiamond(){  
+function totalDiamond() {
   var foundCount = 0;
   foundCount = $(".diamondsweeper-board").find(".found").length;
   $(".total-diamond").text(foundCount);
